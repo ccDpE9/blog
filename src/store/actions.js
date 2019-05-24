3d51be71b0cc87894e9860d27566b1fedeeca243
ccDpE9 <z_eon00@protonmail.com>
2019-05-24 20:44:12 +0200

    More readable

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,15 +4,31 @@
 
 // --- C
 const CREATE_ARTICLE = "CREATE_ARTICLE";
+const CREATE_ARTICLE_SUCCESS = "CREATE_ARTICLE_SUCCESS";
+const CREATE_ARTICLE_FAIL = "CREATE_ARTICLE_FAIL";
+
 
 // --- R
-const ARTICLES_LOADING = "ARTICLES_LOADING";
-const LOAD_ARTICLES_SUCCESS = "LOAD_ARTICLES_SUCCESS";
+const FETCH_ARTICLES = "FETCH_ARTICLES";
+const FETCH_ARTICLES_SUCCESS = "FETCH_ARTICLES_SUCCESS";
+const FETCH_ARTICLES_FAIL = "FETCH_ARTICLES_FAIL";
+
+const FETCH_ARTICLE = "FETCH_ARTICLE";
+const FETCH_ARTICLE_SUCCESS = "FETCH_ARTICLE_SUCCESS";
+const FETCH_ARTICLE_FAIL = "FETCH_ARTICLE_FAIL";
+
 
 // --- U
+const UPDATE_ARTICLE = "UPDATE_ARTICLE";
+const UPDATE_ARTICLE_SUCCESS = "UPDATE_ARTICLE_SUCCESS";
+const UPDATE_ARTICLE_FAIL = "UPDATE_ARTICLE_FAIL";
+
 
 // --- D
-const REMOVE_ARTICLE = "REMOVE_ARTICLE";
+const DELETE_ARTICLE = "DELETE_ARTICLE";
+const DELETE_ARTICLE_SUCCESS = "DELETE_ARTICLE_SUCCESS";
+const DELETE_ARTICLE_FAIL = "DELETE_ARTICLE_FAIL";
+
 
 // --- Filters
 const Filters = {
@@ -29,6 +45,7 @@ const boundCreateArticle = text => dispatch(createArticle(text));
 const boundArticlesAreLoading = (isLoading = true) => dispatch(articlesAreLoading(isLoading));
 const boundArticlesAreLoaded = articles => dispatch(articlesAreLoaded());
 
+ 
 // --- C
 export function createArticle(text) {
   return {
@@ -37,14 +54,62 @@ export function createArticle(text) {
   }
 };
 
+
 // --- R
-export function articlesAreLoading(isLoading = true) {
-  return { type: types.ARTICLES_LOADING, isLoading };
+export function fetchArticles() {
+  const request = axios({
+    method: "get",
+    url: "url",
+    headers: []
+  });
+
+  return {
+    type: FETCH_ARTICLES,
+    payload: request
+  }
 };
 
-export function articlesAreLoaded(articles) {
-  return { type: types.LOAD_ARTICLES_SUCCESS, articles };
+export function fetchArticlesSuccess(articles) {
+  return {
+    type: FETCH_ARTICLES_SUCCESS,
+    payload: articles
+  };
 };
+
+export function fetchArticlesFail(err) {
+  return {
+    type: FETCH_ARTICLES_FAIL,
+    payload: err
+  };
+};
+
+export function fetchArticle() {
+  const request = axios({
+    method: "get",
+    url: "url/id",
+    headers: []
+  });
+
+  return {
+    type: FETCH_ARTICLE,
+    payload: request
+  };
+};
+
+export function fetchArticlesSuccess(article) {
+  return {
+    type: FETCH_ARTICLE_SUCCESS,
+    payload: article
+  };
+};
+
+export function fetchArticleFail(err) {
+  return {
+    type: FETCH_ARTICLE_FAIL,
+    payload: err
+  };
+};
+
 
 // --- U
 // --- D
--- src/store/reducers/articles.js
+++ src/store/reducers/articles.js
@@ -9,20 +9,25 @@ import {
 
 const { SHOW_ALL } = Filters;
 
-export default function articlesReducer(state = initialState, action)
-{
+
+export default function articlesReducer(state = initialState, action) {
   switch (action.type) {
-    case types.ARTICLES_LOADING:
+    // -- C
+      
+    // --- R
+    case FETCH_ARTICLES:
       // @NOTE: You never write directly to states, but return new objects
-      return {
-        ...state,
-        loading: action.isLoading
-      };
-    case types.ARTICLES_LOADED:
-      return {
-        ...state,
-        articles: action.articles
-      };
+      return { ...state, articles: [], error: null, loading: true };
+    case FETCH_ARTICLES_SUCCESS:
+      return { ...state, articles: action.payload, error: null, loading: false };
+    case FETCH_ARTICLES_FAIL:
+      return { ...state, articles: [], error: action.payload, loading: false };
+    case FETCH_ARTICLE:
+      return { ...state, article: null, error: null, loading: true };
+    case FETCH_ARTICLE_SUCCESS:
+      return { ...state, article: action.payload, error: null, loading: false };
+    case FETCH_ARTICLE_FAIL:
+      return { ...state, article: null, error: action.payload, loading: false };
     default:
       return state
   }
