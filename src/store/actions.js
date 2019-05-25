dda307dcab0786c10e3daf3d96ee75424437aa4e
ccDpE9 <z_eon00@protonmail.com>
2019-05-25 20:17:14 +0200

    Copied this function from the docs, that makes action creators functions. Did not export action types since i get this weird message when i try to import them in reducer.

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,35 +3,31 @@
  */
 
 // --- C
-const CREATE_ARTICLE = "CREATE_ARTICLE";
+const CREATE_ARTICLE_REQUEST = "CREATE_ARTICLE_REQUEST";
 const CREATE_ARTICLE_SUCCESS = "CREATE_ARTICLE_SUCCESS";
-const CREATE_ARTICLE_FAIL = "CREATE_ARTICLE_FAIL";
-
-
+const CREATE_ARTICLE_FAILURE = "CREATE_ARTICLE_FAILURE";
 // --- R
-const FETCH_ARTICLES = "FETCH_ARTICLES";
+const FETCH_ARTICLES_REQUEST = "FETCH_ARTICLES_REQUEST";
 const FETCH_ARTICLES_SUCCESS = "FETCH_ARTICLES_SUCCESS";
-const FETCH_ARTICLES_FAIL = "FETCH_ARTICLES_FAIL";
-
-const FETCH_ARTICLE = "FETCH_ARTICLE";
+const FETCH_ARTICLES_FAILURE = "FETCH_ARTICLES_FAILURE";
+const FETCH_ARTICLE_REQUEST = "FETCH_ARTICLE_REQUEST";
 const FETCH_ARTICLE_SUCCESS = "FETCH_ARTICLE_SUCCESS";
-const FETCH_ARTICLE_FAIL = "FETCH_ARTICLE_FAIL";
-
-
+const FETCH_ARTICLE_FAILURE = "FETCH_ARTICLE_FAILURE";
 // --- U
-const UPDATE_ARTICLE = "UPDATE_ARTICLE";
+const UPDATE_ARTICLE_REQUEST = "UPDATE_ARTICLE_REQUEST";
 const UPDATE_ARTICLE_SUCCESS = "UPDATE_ARTICLE_SUCCESS";
-const UPDATE_ARTICLE_FAIL = "UPDATE_ARTICLE_FAIL";
-
-
+const UPDATE_ARTICLE_FAILURE = "UPDATE_ARTICLE_FAILURE";
 // --- D
-const DELETE_ARTICLE = "DELETE_ARTICLE";
+const DELETE_ARTICLE_REQUEST = "DELETE_ARTICLE_REQUEST";
 const DELETE_ARTICLE_SUCCESS = "DELETE_ARTICLE_SUCCESS";
-const DELETE_ARTICLE_FAIL = "DELETE_ARTICLE_FAIL";
+const DELETE_ARTICLE_FAILURE = "DELETE_ARTICLE_FAILURE";
 
 
-// --- Filters
-const Filters = {
+/*
+ * --- OTHER CONSTANTS ---
+ */
+
+export const Filters = {
   SHOW_ALL: "SHOW_ALL",
 };
 
@@ -40,76 +36,35 @@ const Filters = {
  * --- ACTION CREATORS ---
  */
 
-// --- Bound action creators
-const boundCreateArticle = text => dispatch(createArticle(text));
-const boundArticlesAreLoading = (isLoading = true) => dispatch(articlesAreLoading(isLoading));
-const boundArticlesAreLoaded = articles => dispatch(articlesAreLoaded());
+function makeActionCreator(type, ...argNames) {
+  return function(...args) {
+    const action = { type };
+    argNames.forEach((arg, index) => {
+      action[argNames[index]] = args[index];
+    });
+    return action
+  }
+};
 
- 
+
 // --- C
-export function createArticle(text) {
-  return {
-    type: CREATE_ARTICLE,
-    text
-  }
-};
-
-
+export const createArticleRequest = makeActionCreator(CREATE_ARTICLE_REQUEST, "text");
 // --- R
-export function fetchArticles() {
-  const request = axios({
-    method: "get",
-    url: "url",
-    headers: []
-  });
-
-  return {
-    type: FETCH_ARTICLES,
-    payload: request
-  }
-};
-
-export function fetchArticlesSuccess(articles) {
-  return {
-    type: FETCH_ARTICLES_SUCCESS,
-    payload: articles
-  };
-};
-
-export function fetchArticlesFail(err) {
-  return {
-    type: FETCH_ARTICLES_FAIL,
-    payload: err
-  };
-};
-
-export function fetchArticle() {
-  const request = axios({
-    method: "get",
-    url: "url/id",
-    headers: []
-  });
-
-  return {
-    type: FETCH_ARTICLE,
-    payload: request
-  };
-};
-
-export function fetchArticlesSuccess(article) {
-  return {
-    type: FETCH_ARTICLE_SUCCESS,
-    payload: article
-  };
-};
-
-export function fetchArticleFail(err) {
-  return {
-    type: FETCH_ARTICLE_FAIL,
-    payload: err
-  };
-};
-
-
+export const fetchArticlesRequest = makeActionCreator(FETCH_ARTICLES);
+export const fetchArticlesSuccess = makeActionCreator(FETCH_ARTICLES_SUCCESS, articles);
+export const fetchArticlesFailure = makeActionCreator(FETCH_ARTICLES_FAILURE, error);
+export const fetchArticleRequest = makeActionCreator(FETCH_ARTICLE, article);
+export const fetchArticleSuccess = makeActionCreator(FETCH_ARTICLE_SUCCESS, article);
+export const fetchArticleFailure = makeActionCreator(FETCH_ARTICLE_FAILURE, error);
 // --- U
+export const updateArticleRequest = makeActionCreator(UPDATE_ARTICLE_REQUEST);
+export const updateArticleSuccess = makeActionCreator(UPDATE_ARTICLE_SUCCESS, article);
+export const updateArticleFailure = makeActionCreator(UPDATE_ARTICLE_FAILURE, error);
 // --- D
+export const deleteArticleRequest = makeActionCreator(DELETE_ARTICLE_REQUEST);
+export const deleteArticleSuccess = makeActionCreator(DELETE_ARTICLE_SUCCESS);
+export const deleteArticleFailure = makeActionCreator(DELETE_ARTICLE_FAILURE, error);
+
+
+// --- Bound action creators
+const bound{action} = (arg) => dispatch({action}(arg));
