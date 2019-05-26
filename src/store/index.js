3e9d3e73f326472a5c17f8305e51094f99f55bf0
ccDpE9 <z_eon00@protonmail.com>
2019-05-26 23:10:20 +0200

    More refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./reducers/index";
 import thunk from "redux-thunk";
 
-export function configureStore(initialState) {
-  return createStore(
+const configureStore = () => {
+  const store = createStore(
     rootReducer,
     initialState,
     applyMiddleware(
       thunk
     )
   )
+
+  return store;
 };
+
+export default configureStore;
