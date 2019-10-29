7b085589db3307d1dc32d7b033a5a40b5bd90fe1
ccDpE9 <z_eon00@protonmail.com>
2019-10-29 18:36:06 +0100

    Query for all articles, and link to each.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 interface IndexPageProps {
-  data: {
-    site: {
-      siteMetadata: {
-	title: string;
-      }
-    }
-  }
 }
 
-const App: React.SFC<IndexPageProps> = (props) => {
+const App: React.SFC<IndexPageProps> = ({ data }) => {
+  const articles = data.allMarkdownRemark.edges
+
   return (
     <Layout>
+      <h4>{ data.allMarkdownRemark.totalCount }</h4>
       <section className="articles">
-	{props.data.allFile.edges.map(({ node }, index) => (
+	{articles.map(({ node }) => (
+	<div id={ node.id }>
+	  <p>One article</p>
+	  <Link to={node.fields.slug}>
+	    <h3>
+	      { node.frontmatter.title }
+	      <span>- { node.frontmatter.date }</span>
+	    </h3>
+	    <p>{ node.exceprt }</p>
+	  </Link>
+	</div>
 	))}
       </section>
     </Layout>
@@ -28,22 +34,24 @@ const App: React.SFC<IndexPageProps> = (props) => {
 
 export default App
 
-export const pageQuery = graphql`
-query {
-  allMarkdownRemark(sort: { fields: [frontmatter__date], order: DESC }) {
-    edges {
-      node {
-	excerpt
-	fields {
-	  slug
-	}
-	frontmatter {
-	  date(formatString: "MMMMM DD, YYYY")
-	  title
-	  description
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+	node {
+	  id
+	  frontmatter {
+	    title
+	    description
+	    date(formatString: "DD MMMM, YYYY")
+	  }
+	  fields {
+	    slug
+	  }
+	  excerpt
 	}
       }
     }
-  }
 }
 `
