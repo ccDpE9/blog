c2195fbb50bce22bd4edef97850ec5f9814e0c69
ccDpE9 <z_eon00@protonmail.com>
2019-11-08 23:40:57 +0100

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,17 @@ const App: React.SFC<IndexPageProps> = ({ data }) => {
       <h4>{ data.allMarkdownRemark.totalCount }</h4>
       <section className="articles">
 	{articles.map(({ node }, index) => (
-	<div id={ node.id }>
+	<div classname="articles__article" id={ node.id }>
 	  <Link to={node.fields.slug}>
-	    <h3>{ index + 1}. { node.frontmatter.title }</h3>
-	    <span>{node.frontmatter.date }</span>
+	    <h3 className="articles__title">{ index + 1}. { node.frontmatter.title }</h3>
+	    <span className="articles__date">{ node.frontmatter.date } | </span>
+	    {node.frontmatter.tags ? (
+	      <div className="articles__tags">
+		{node.frontmatter.tags.map(tag => (
+		<span className="articles__tag">{tag}</span>
+		))}
+	      </div>
+	    ) : null }
 	  </Link>
 	</div>
 	))}
@@ -41,6 +48,7 @@ export const query = graphql`
 	    title
 	    description
 	    date(formatString: "DD-MM-YYYY")
+	    tags
 	  }
 	  fields {
 	    slug
--- src/pages/test.md
+++
@@ -1,6 +0,0 @@
----
-title: "Just a test file"
-date: "2019-09-10"
----
-
-This is really complicated.
