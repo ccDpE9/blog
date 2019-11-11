c62b39abb857457262ad724041aa8cf4688e5ae9
ccDpE9 <z_eon00@protonmail.com>
2019-11-11 15:19:58 +0100

    Basic pagination for both posts in general, as well as tags.

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,6 +37,22 @@ exports.createPages = async ({ graphql, actions }) => {
 
   const articles = result.data.allMarkdownRemark.edges;
 
+  // Create blog-list pages
+  const postsPerPage = 6
+  const numPages = Math.ceil(articles.length / postsPerPage)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/` : `/page-${i+1}`,
+      component: path.resolve("./src/templates/Articles.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      }
+    })
+  })
+
   // Create a page for each article
   articles.forEach(({ node }) => {
     createPage({
@@ -50,20 +66,41 @@ exports.createPages = async ({ graphql, actions }) => {
 
   // Create a page for each tag
   let tags = [];
-
   articles.forEach(({ node }) => {
     node.frontmatter.tags.forEach(tag => {
       tags.includes(tag) ? null : tags.push(tag);
     })
   })
 
-  tags.forEach(tag => {
-    createPage({
-      path: `/tags/${tag}/`,
-      component: path.resolve("./src/templates/Tags.js"),
-      context: {
-        tag
+  let counts = {}
+  articles.forEach(article => {
+    article.node.frontmatter.tags.forEach(tag => {
+      if (!counts.hasOwnProperty(tag)) {
+        counts[tag] = {}
+        counts[tag].numPosts = 1
+      } else {
+        counts[tag].numPosts += 1
       }
+    })
+  })
+  for (let [tag] of Object.entries(counts)) {
+    counts[tag].numPages = Math.ceil(counts[tag].numPosts / postsPerPage)
+  }
+
+  tags.forEach(tag => {
+    Array.from({ length: counts[tag].numPages }).forEach((_, i) => {
+      createPage({
+        path: `/tags/${tag}/`,
+        path: i === 0 ? `/tags/${tag}` : `/tags/${tag}-${i+1}`,
+        component: path.resolve("./src/templates/Tags.js"),
+        context: {
+          tag,
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages: counts[tag].numPages,
+          currentPage: i + 1,
+        }
+      })
     })
   })
 }
--- src/templates/Articles.js
+++ src/templates/Articles.js
@@ -2,37 +2,36 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/Layout"
 
-const Tags = ({ pageContext, data }) => {
-  const { tag } = pageContext
+const App = ({ pageContext, data }) => {
   const { edges, totalCount } = data.allMarkdownRemark
-  const tagHeader = `${totalCount} post${
+  const tagHeader = `Total post${
     totalCount === 1 ? "" : "s"
-  } tagged with "${tag}"`
+  } number: ${totalCount}`
   const { currentPage, numPages } = pageContext
   const isFirst = currentPage === 1
   const isLast = currentPage - 1 === numPages
-  const prevPage = currentPage - 1 === 1 ? `/tags/${tag}` : `/tags/${tag}/-${(currentPage - 1).toString()}`
-  const nextPage = `/tags/${tag}-${(currentPage + 1).toString()}`
+  const prevPage = currentPage - 1 === 1 ? "/" : `/page-${(currentPage - 1).toString()}`
+  const nextPage = `/page-${(currentPage + 1).toString()}`
 
   return (
     <Layout>
       <section className="articles">
-        <h4 className="articles__header">{ tagHeader }</h4>
+        <h4>{ tagHeader }</h4>
         {edges.map(({ node }, index) => (
-          <div classname="articles__article" id={ node.id }>
+          <div className="articles__article" id={ node.id }>
             <Link to={node.fields.slug}>
               <h3 className="articles__title">{ index + 1}. { node.frontmatter.title }</h3>
+              <span className="articles__date">{ node.frontmatter.date } | </span>
+              {node.frontmatter.tags ? (
+                <div className="articles__tags">
+                  {node.frontmatter.tags.map(tag => (
+                    <Link to={`/tags/${tag}`}>
+                      <span className="articles__tag">{tag}</span>
+                    </Link>
+                  ))}
+                </div>
+              ) : null }
             </Link>
-            <span className="articles__date">{ node.frontmatter.date } | </span>
-            {node.frontmatter.tags ? (
-              <div className="articles__tags">
-                {node.frontmatter.tags.map(tag => (
-                  <Link to={`tags/${tag}`}>
-                    <span className="articles__tag">{tag}</span>
-                  </Link>
-                ))}
-              </div>
-            ) : null }
           </div>
         ))}
         {!isFirst && (
@@ -41,7 +40,7 @@ const Tags = ({ pageContext, data }) => {
           </Link>
         )}
         {Array.from({ length: numPages }, (_, i) => (
-          <Link key={`pagination-number${i+1}`} to={i === 0 ? `/tags/${tag}` : `/tags/${tag}-${i + 1}`}>
+          <Link key={`pagination-number${i+1}`} to={i === 0 ? "/" : `/page-${i + 1}`}>
             {i + 1}
           </Link>
         ))}
@@ -55,31 +54,29 @@ const Tags = ({ pageContext, data }) => {
   )
 }
 
-export default Tags
+export default App
 
 export const query = graphql`
-  query(
-    $tag: String,
-    $limit: Int!,
-    $skip: Int!
-  ) {
+  query ($skip: Int!, $limit: Int!) {
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { tags: { in: [$tag] } } }
       limit: $limit
       skip: $skip
     ) {
       totalCount
       edges {
         node {
-          fields {
-            slug 
-          }
+          id
           frontmatter {
             title
+            description
             date(formatString: "DD-MM-YYYY")
             tags
           }
+          fields {
+            slug
+          }
+          excerpt
         }
       }
     }
