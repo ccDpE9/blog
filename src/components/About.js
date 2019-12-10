417f7b347fd45c9fc2de3b9d5e7293853262d0f6
ccDpE9 <z_eon00@protonmail.com>
2019-12-10 23:25:32 +0100

    init About component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Image from "gatsby-image";
+
+export default () => (
+  <section className="about">
+    <Image />
+    <p>
+      Placeholder
+    </p>
+  </section>
+)
